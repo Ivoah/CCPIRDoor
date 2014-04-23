57b30fc98cb3ed23c7242e8fea8f0f3dd7ab4ed6
Ivoah <ivoahivoah@gmail.com>
2014-04-22 21:30:30 -0400

    Create PIR.py

---
+++ PIR.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# This program goes on the Raspberry Pi
+
+from RPi.GPIO import *
+from time import sleep
+from BaseHTTPServer import BaseHTTPRequestHandler
+
+setmode(BCM)
+
+SIG = 17
+VCC = 27
+GND = 22
+
+setup(SIG, IN)
+setup(VCC, OUT)
+setup(GND, OUT)
+
+output(VCC, HIGH)
+output(GND, LOW)
+
+class PIRRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.end_headers()
+        self.wfile.write(input(SIG))
+
+if __name__ == '__main__':
+    from BaseHTTPServer import HTTPServer
+    server = HTTPServer(('', 3141), PIRRequestHandler)
+    print 'Serving on port 3141'
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    cleanup()
+    print('\nBye!')
